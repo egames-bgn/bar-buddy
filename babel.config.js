84380baea2e055415eacb8424fd8afb8fa07b0d1
SeanEgames <sburke@egames.com>
2025-08-11 11:36:59 -0700

    IOS changes

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,22 @@
-module.exports = function(api) { 
-  api.cache(true); 
-  return { 
-    presets: ['babel-preset-expo'], 
-    plugins: [ 
-      ['module-resolver', { 
-        root: ['.'], 
-        alias: { 
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        root: ['.'],
+        alias: {
           '@': '.',
           '~': './src',
-        }, 
-      }], 
-      'expo-router/babel', 
-      'react-native-reanimated/plugin',
+        },
+      }],
       ['transform-inline-environment-variables', {
         include: [
           'NODE_ENV',
           'REACT_APP_*',
         ]
       }],
-    ], 
-  }; 
-}; 
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
